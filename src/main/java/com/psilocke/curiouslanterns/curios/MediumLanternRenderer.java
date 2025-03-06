607d306b52901607477f62c2a23727b380f0fa03
Rhys <rhysculley10@gmail.com>
2025-03-05 23:50:03 -0800

    resolved misplaced lanterns for most mods

--- src/main/java/com/psilocke/curiouslanterns/curios/MediumLanternRenderer.java
+++ src/main/java/com/psilocke/curiouslanterns/curios/MediumLanternRenderer.java
@@ -17,6 +17,7 @@ import net.minecraft.util.Mth;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.LanternBlock;
 import top.theillusivec4.curios.api.SlotContext;
 import top.theillusivec4.curios.api.client.ICurioRenderer;
 
@@ -65,7 +66,7 @@ public class MediumLanternRenderer implements ICurioRenderer {
             matrixStack.mulPose(Vector3f.ZP.rotationDegrees(-f3 / 2.0F));
 		}
 		matrixStack.scale(0.33f, 0.33f, 0.33f);
-		BakedModel lantern = blockRenderer.getBlockModel(Block.byItem(stack.getItem()).defaultBlockState());
+		BakedModel lantern = blockRenderer.getBlockModel(Block.byItem(stack.getItem()).defaultBlockState().setValue(LanternBlock.HANGING, true));
 		MultiBufferSource.BufferSource buffer = Minecraft.getInstance().renderBuffers().bufferSource();
 		itemRenderer.render(stack, ItemTransforms.TransformType.HEAD, true, matrixStack, buffer, light, OverlayTexture.NO_OVERLAY, lantern);
 		matrixStack.popPose();
